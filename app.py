78b3c5c8ec8211b20178ceef3fbe78bab89c923f
kfpro <njrfarhandasilva10@gmail.com>
2021-05-04 22:18:29 +0530

    complete app with db

--- app.py
+++ app.py
@@ -10,6 +10,32 @@ from flask_apscheduler import APScheduler
 
 scheduler = APScheduler()
 
+def openTXT(path="userdata.txt"):
+    with open(path) as f:
+        data = f.readlines()
+    return data
+
+def writeTXT(line,path="userdata.txt"):
+    with open(path, "a+") as f:
+        f.write(line+"\n")
+
+# for regular expressions
+import re
+ 
+# Make a regular expression
+# for validating an Email
+regex_mail = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
+
+ 
+def isValidEmail(email):
+ 
+    # pass the regular expression
+    # and the string in search() method
+    if(re.search(regex_mail, email)):
+        return True
+ 
+    else:
+        return False
 
 # "templates" this is for plain html files or "Great_Templates" this is for complex css + imgs +js +html+sass
 TEMPLATES = "templates"
@@ -24,16 +50,27 @@ def notify_user(mail = 'swaymsdennings@gmail.com',DIST_ID = 446, numdays = 20, a
     flag_available, Available_Slots = getSlots(DIST_ID, numdays, age)
     msg = "No available slots found"
     body = convert_to_str(Available_Slots) if len(Available_Slots) > 0 else msg
-    send_mail(body, receiver_email=mail,subject='VACCINE AVAILABILITY NOTIFICATION')
+    print(mail,body,"Sent!")
+    if len(Available_Slots) > 0 :
+        send_mail(body, receiver_email=mail,subject='VACCINE AVAILABILITY NOTIFICATION')
 
 def scheduleTask():
-    print("This test runs every 3 seconds")
+    data = openTXT()
+    for each_line in data:
+        emailid,districtid,age,days=each_line.split(",")
+        districtid,age,days = int(districtid),int(age),int(days)
+        notify_user(emailid,districtid,age,days)
+    print("Scheduler Running")
 
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route("/view")
+def view():
+    return openTXT()
 
 
 @app.route('/register', methods=["GET", "POST"])
@@ -47,14 +84,26 @@ def register():
         
         print(emailid,districtid,age,days,conds)
         
-        
+        if not conds=="on" or not isValidEmail(emailid):
+            raise ValueError('Terms and Conditions not met or Invalid Email ID.')
+        line = emailid + "," + str(districtid) + "," + str(age) + "," + str(days)
+        writeTXT(line)
         return render_template('register.html')
-    except:
-        return "Incorrect Data Entered."
+    except Exception as err:
+        err_message = f"{err.__class__.__name__}: {err}"
+        return "Incorrect Data Entered.\n" + err_message
+"""
+emailid,districtid,age,days= "njrfarhandasilva10@gmail.com",255,21,20
+line = emailid + "," + str(districtid) + "," + str(age) + "," + str(days)
+writeTXT(line)
+emailid,districtid,age,days= "swaymsdennings@gmail.com",446,21,20
+line = emailid + "," + str(districtid) + "," + str(age) + "," + str(days)
+writeTXT(line)
+"""
 
 
 if __name__ == "__main__":
-    scheduler.add_job(id = 'Scheduled Task', func=scheduleTask, trigger="interval", seconds=18000)
+    scheduler.add_job(id = 'Scheduled Task', func=scheduleTask, trigger="interval", seconds=6000,max_instances=10) # 18000
     scheduler.start()
     app.run()
 """
