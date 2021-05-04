c19143589e23782a3c282b08a3968c28b7236c31
kfpro <njrfarhandasilva10@gmail.com>
2021-05-04 21:30:25 +0530

    add scheduler

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from flask import Flask, render_template, request, url_for, send_from_directory,
 import os
 from vaccine_availability import getSlots, convert_to_str, send_mail
 
+from flask_apscheduler import APScheduler
+
+scheduler = APScheduler()
+
 
 # "templates" this is for plain html files or "Great_Templates" this is for complex css + imgs +js +html+sass
 TEMPLATES = "templates"
@@ -16,26 +20,42 @@ ROOT_DIR = os.getcwd()
 # Reloading
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
+def notify_user(mail = 'swaymsdennings@gmail.com',DIST_ID = 446, numdays = 20, age = 21):
+    flag_available, Available_Slots = getSlots(DIST_ID, numdays, age)
+    msg = "No available slots found"
+    body = convert_to_str(Available_Slots) if len(Available_Slots) > 0 else msg
+    send_mail(body, receiver_email=mail,subject='VACCINE AVAILABILITY NOTIFICATION')
+
+def scheduleTask():
+    print("This test runs every 3 seconds")
+
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
-@app.route('/register', METHODS=["GET", "POST"])
+@app.route('/register', methods=["GET", "POST"])
 def register():
     try:
-        form_title = request.form["title"]
-        form_year = request.form["year"]
-        movie = Movie(form_title, year=int(form_year) if form_year else None)
-        db = current_app.config["db"]
-        movie_key = db.add_movie(movie)
-        return redirect(url_for("movie_page", movie_key=movie_key))
+        emailid = request.form["emailid"]
+        districtid = int(request.form["districtid"])
+        age = int(request.form["age"])
+        days = int(request.form["days"])
+        conds = request.form["conds"]
+        
+        print(emailid,districtid,age,days,conds)
+        
+        
+        return render_template('register.html')
     except:
         return "Incorrect Data Entered."
 
 
 if __name__ == "__main__":
+    scheduler.add_job(id = 'Scheduled Task', func=scheduleTask, trigger="interval", seconds=18000)
+    scheduler.start()
     app.run()
 """
 if __name__ == '__main__':
